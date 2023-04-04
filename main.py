c6408bc20a23b3fea4c5f79d38c2e25db1f5b099
Anshu Sinha <anshujuly2@gmail.com>
2023-04-04 10:02:50 +0530

    Add a code to read, write and append texts in a file

---
+++ main.py
@@ -0,0 +1,16 @@
+# """reading a file"""
+# with open("file1.txt") as file:
+#     contents = file.read()
+#     print(contents)
+
+# """writing in a file"""
+# with open("file1.txt", mode="w") as file:
+#     file.write("I'm a python developer")
+
+# """appending to a file"""
+# with open("file1.txt", mode="a") as file:
+#     file.write("\nMy name is Anshu")
+
+"""creating a new file"""
+with open ("file2.txt", mode="w") as file:
+    file.write("This is the file2 created!")
